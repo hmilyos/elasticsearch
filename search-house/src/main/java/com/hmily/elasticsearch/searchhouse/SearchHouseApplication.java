b5212d1bd9364273daf1340155cc11ffb536e6a6
oushiming <oushiming@sxt.com>
2018-11-26 23:55:52 +0800

    init

---
+++ search-house/src/main/java/com/hmily/elasticsearch/searchhouse/SearchHouseApplication.java
@@ -0,0 +1,12 @@
+package com.hmily.elasticsearch.searchhouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SearchHouseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SearchHouseApplication.class, args);
+	}
+}
